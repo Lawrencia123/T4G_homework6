5a05c8b9ed20539256db22fb74eb651801818a38
Lawrencia Baaba Mensah <lawbabmensah@gmail.com>
2024-12-02 09:53:05 +0000

    added new files

---
+++ src/index.js
@@ -0,0 +1,18 @@
+let fName = "Lawrencia"; mName = "Baaba"); lName = ("Mensah");
+console.log(myName);
+let age = 22;
+console.log(age);
+let agesum = 22 + 11;
+console.log(agesum);
+let deduction = 33-12;
+console.log(deduction);
+let divid = 45 / 5;
+console.log(divid);
+let mutiply = 11 * 11
+console.log(multiply);
+let remainder = 15 % 4;
+console.log(remainder); 
+let student = false;
+console.log(student);
+let x = 5
+console.log()
